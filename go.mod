19e32c218052243b753c03aa5004ee9feeb405f8
Ansel Sulejmani <anselsulejmani826@gmail.com>
2025-08-02 18:18:22 -0600

    set up env system

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module wikiBot
 
 go 1.24.5
 
-require github.com/bwmarrin/discordgo v0.29.0
+require (
+	github.com/bwmarrin/discordgo v0.29.0
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
--- src/main.go
+++ src/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	bot "wikiBot/src/bot"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	bot.BotToken = ""
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("ERROR LOADING ENV")
+		return
+	}
+	bot.BotToken = os.Getenv("DISCORD_BOT_TOKEN")
 	bot.Run() // simply call for the bot to be ran
-
 }
